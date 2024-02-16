5741fe313d12d0af26cd6201325dd8326616376b
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2024-02-16 13:47:44 +0100

    Initial Commit

---
+++ RickAndMortyTask/RickAndMortyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTaskApp.swift
+//  RickAndMortyTask
+//
+//  Created by Patka on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
